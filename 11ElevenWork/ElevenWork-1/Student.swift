c9edba3bc1f8ab149a5abe28e6077217d6554ae9
zhangzongping <zhangzongping@student.local>
2018-12-18 15:36:08 +0800

    zhangzongping_2016110353

---
+++ 11ElevenWork/ElevenWork-1/Student.swift
@@ -0,0 +1,14 @@
+//
+//  Student.swift
+//  ElevenWork
+//
+//  Created by zhangzongping on 2018/12/17.
+//  Copyright © 2018年 zongping.zhang. All rights reserved.
+//
+
+import Foundation
+class Student {
+    var name = ""
+    var no = ""
+    var age = ""
+}
---
+++ 11ElevenWork/ElevenWork-1/ThirdViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ThirdViewController.swift
+//  ElevenWork
+//
+//  Created by zhangzongping on 2018/12/17.
+//  Copyright © 2018年 zongping.zhang. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
